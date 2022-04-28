517ff82ebbfe1afc6eda393c5c1878937562c166
Alvaromorenogil <amorgil0905@g.educaand.es>
2022-04-28 10:53:18 +0200

    AMG csv - 28/04/2022

---
+++ ProyectoCSV/ProyectoCSV/src/main/java/com/alvaromorenogil/proyectocsv/Datos.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.alvaromorenogil.proyectocsv;
+
+/**
+ *
+ * @author usuario
+ */
+public class Datos {
+    
+    private String pais;
+    private String codPais;
+    private String reporteBullying;
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getCodPais() {
+        return codPais;
+    }
+
+    public void setCodPais(String codPais) {
+        this.codPais = codPais;
+    }
+
+    public String getReporteBullying() {
+        return reporteBullying;
+    }
+
+    public void setReporteBullying(String reporteBullying) {
+        this.reporteBullying = reporteBullying;
+    }
+    
+    
+    
+}
